d28b3ea34372e42d3e7705fe335a86939c93f38f
Sid Price <sid@sidprice.com>
2025-05-06 12:53:01 -0600

    ctxlink/spi_protocol: Refactor and add split return whole packet count.

--- protocol.c
+++ protocol.c
@@ -13,7 +13,7 @@
 
 #include "protocol.h"
 
-#define PACKET_HEADER_SIZE PACKET_HEADER_DATA_START;  // Size of the header before the data starts
+#define PACKET_HEADER_SIZE PACKET_HEADER_DATA_START; // Size of the header before the data starts
 
 /**
  * @brief Package the passed data into a buffer for transmission
@@ -33,33 +33,36 @@
  *      Byte (n-1)+5    : Last data byte
  * 
  */
-size_t package_data(uint8_t * buffer, size_t data_length, protocol_packet_type_e data_type, size_t buffer_size) {
-    memmove(buffer + PACKET_HEADER_DATA_START, buffer, data_length);  // Move the data to start 3 bytes in
+size_t package_data(uint8_t *buffer, size_t data_length, protocol_packet_type_e data_type)
+{
+	memmove(buffer + PACKET_HEADER_DATA_START, buffer, data_length); // Move the data to start 3 bytes in
 
-    size_t packet_length = data_length + PACKET_HEADER_SIZE;  // Total packet length
-    size_t padding = packet_length % 4;  // Calculate padding to make the total size a multiple of 4 
-    buffer[PACKET_HEADER_MAGIC1] = PROTOCOL_MAGIC1;  // First byte of magic number
-    buffer[PACKET_HEADER_MAGIC2] = PROTOCOL_MAGIC2;  // Second byte of magic number
-    buffer[PACKET_HEADER_SOURCE_ID] = data_type;  // Message source identifier
-    buffer[PACKET_HEADER_LENGTH_MSB] = (data_length >> 8) & 0xFF;  // High byte of data length
-    buffer[PACKET_HEADER_LENGTH_LSB] = data_length & 0xFF;         // Low byte of data length
-    if (padding > 0) {
-        padding = 4 - padding;  // Calculate the number of padding bytes needed
-        memset(buffer + packet_length, 0x00, padding);  // Fill the padding bytes with 0x00
-    }
-    return packet_length + padding;  // Return the total size of the packet including header
+	size_t packet_length = data_length + PACKET_HEADER_SIZE; // Total packet length
+	size_t padding = packet_length % 4;                      // Calculate padding to make the total size a multiple of 4
+	buffer[PACKET_HEADER_MAGIC1] = PROTOCOL_MAGIC1;          // First byte of magic number
+	buffer[PACKET_HEADER_MAGIC2] = PROTOCOL_MAGIC2;          // Second byte of magic number
+	buffer[PACKET_HEADER_SOURCE_ID] = data_type;             // Message source identifier
+	buffer[PACKET_HEADER_LENGTH_MSB] = (data_length >> 8) & 0xFF; // High byte of data length
+	buffer[PACKET_HEADER_LENGTH_LSB] = data_length & 0xFF;        // Low byte of data length
+	if (padding > 0) {
+		padding = 4 - padding;                         // Calculate the number of padding bytes needed
+		memset(buffer + packet_length, 0x00, padding); // Fill the padding bytes with 0x00
+	}
+	return packet_length + padding; // Return the total size of the packet including header
 }
 
 /**
  * @brief Split the passed protocol packet into its component parts
  * 
  * @param message       Pointer to the protocol packet
- * @param packet_size   Pointer to a register to receive the packet data size
+ * @param data_length   Pointer to a register to receive the packet data size
  * @param packet_type   Pointer to a register to receive the packet type
  * @param data          Pointer to a register to receive the pointer to the packet data content
  */
-void protocol_split(uint8_t *message, size_t *packet_size, protocol_packet_type_e *packet_type, uint8_t **data) {
-    *packet_size = (message[PACKET_HEADER_LENGTH_MSB] << 8) | message[PACKET_HEADER_LENGTH_LSB];  // Get the packet size
-    *packet_type = (protocol_packet_type_e)message[PACKET_HEADER_SOURCE_ID];  // Get the packet type
-    *data = message + PACKET_HEADER_DATA_START;  // Get the data pointer
+size_t protocol_split(uint8_t *message, size_t *data_length, protocol_packet_type_e *packet_type, uint8_t **data)
+{
+	*data_length = (message[PACKET_HEADER_LENGTH_MSB] << 8) | message[PACKET_HEADER_LENGTH_LSB]; // Get the packet size
+	*packet_type = (protocol_packet_type_e)message[PACKET_HEADER_SOURCE_ID];                     // Get the packet type
+	*data = message + PACKET_HEADER_DATA_START;                                                  // Get the data pointer
+	return *data_length + PACKET_HEADER_DATA_START; // Return the packet size
 }
--- protocol.h
+++ protocol.h
@@ -11,22 +11,22 @@
  * communications between the ESP32 and ctxLink
  */
 
- #ifndef PROTOCOL_H
- #define PROTOCOL_H
- 
- #ifdef __cplusplus
- extern "C" {
- #endif
- 
- #ifdef ESP32_BUILD
-     #include <Arduino.h>
- #else
-     #include <stdint.h>
-     #include <stdlib.h>
-     #include <string.h>
- #endif
- 
- /*
+#ifndef PROTOCOL_H
+#define PROTOCOL_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef ESP32_BUILD
+#include <Arduino.h>
+#else
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#endif
+
+/*
      This file describes the packet protocol used between an ESP32 based
      Wi-FI module and ctxLink using SPI peripherals.
  
@@ -46,43 +46,42 @@
              required by the ESP32 DMA peripheral. If required, dummy
              bytes of 0x00 should be appended.
  */
- 
- /**
+
+/**
   * @brief Protocol Packet Header offsets
   * 
   */
- enum packet_header_offset_e {
-     PACKET_HEADER_MAGIC1 = 0,  // First byte of the magic number
-     PACKET_HEADER_MAGIC2,      // Second byte of the magic number
-     PACKET_HEADER_SOURCE_ID,   // Message source identifier (e.g. 0x01 for GDB
-     PACKET_HEADER_LENGTH_MSB,  // Most significant byte of the length of the data
-     PACKET_HEADER_LENGTH_LSB,  // Least significant byte of the length of the data)
-     PACKET_HEADER_DATA_START   // Start of the actual data in the packet 
- } ;
- 
- 
- /**
+enum packet_header_offset_e {
+	PACKET_HEADER_MAGIC1 = 0, // First byte of the magic number
+	PACKET_HEADER_MAGIC2,     // Second byte of the magic number
+	PACKET_HEADER_SOURCE_ID,  // Message source identifier (e.g. 0x01 for GDB
+	PACKET_HEADER_LENGTH_MSB, // Most significant byte of the length of the data
+	PACKET_HEADER_LENGTH_LSB, // Least significant byte of the length of the data)
+	PACKET_HEADER_DATA_START  // Start of the actual data in the packet
+};
+
+/**
   * @brief Definition of the protocol magic number, used in the header
   * 
   */
- #define PROTOCOL_MAGIC1 0xDE ; // First byte of the magic number
- #define PROTOCOL_MAGIC2 0xAD ; // Second byte of the magic number
- 
- typedef enum {
-     PROTOCOL_PACKET_TYPE_EMPTY = 0x00,      // 0x00 Empty packet type 
-     PROTOCOL_PACKET_TYPE_CMD,               // 0x01 Command from ctxLink
-     PROTOCOL_PACKET_TYPE_STATUS,            // 0x02 Status of ESP32 to ctxLink
-     PROTOCOL_PACKET_TYPE_FROM_GDB,          // 0x03 Packet from GDB
-     PROTOCOL_PACKET_TYPE_TO_GDB,            // 0x04 Packet to GDB
-     PROTOCOL_PACKET_TYPE_FROM_CTXLINK,      // 0x05 Packet from ctxLink
-     PROTOCOL_PACKET_TYPE_TO_CTXLINK,        // 0x06 Packet to ctxLink
-     PROTOCOL_PACKET_TYPE_SWO,               // 0x07 SWO packet type - to network
-     PROTOCOL_PACKET_TYPE_UART_FROM_CTXLINK, // 0x08 UART packet from ctxLink
-     PROTOCOL_PACKET_TYPE_UART_TO_CTXLINK,   // 0x09 UART packet to ctxLink
-     PROTOCOL_PACKET_TYPE_NETWORK_INFO,          // 0x0a Network information packet type
- } protocol_packet_type_e;
- 
- /*
+#define PROTOCOL_MAGIC1 0xDE; // First byte of the magic number
+#define PROTOCOL_MAGIC2 0xAD; // Second byte of the magic number
+
+typedef enum {
+	PROTOCOL_PACKET_TYPE_EMPTY = 0x00,      // 0x00 Empty packet type
+	PROTOCOL_PACKET_TYPE_CMD,               // 0x01 Command from ctxLink
+	PROTOCOL_PACKET_TYPE_STATUS,            // 0x02 Status of ESP32 to ctxLink
+	PROTOCOL_PACKET_TYPE_FROM_GDB,          // 0x03 Packet from GDB
+	PROTOCOL_PACKET_TYPE_TO_GDB,            // 0x04 Packet to GDB
+	PROTOCOL_PACKET_TYPE_FROM_CTXLINK,      // 0x05 Packet from ctxLink
+	PROTOCOL_PACKET_TYPE_TO_CTXLINK,        // 0x06 Packet to ctxLink
+	PROTOCOL_PACKET_TYPE_SWO,               // 0x07 SWO packet type - to network
+	PROTOCOL_PACKET_TYPE_UART_FROM_CTXLINK, // 0x08 UART packet from ctxLink
+	PROTOCOL_PACKET_TYPE_UART_TO_CTXLINK,   // 0x09 UART packet to ctxLink
+	PROTOCOL_PACKET_TYPE_NETWORK_INFO,      // 0x0a Network information packet type
+} protocol_packet_type_e;
+
+/*
      Define the format of the ESP32 status packet to ctxLink
      
      The status packet is used to inform ctxLink of the status of the ESP32, it has the following format:
@@ -97,41 +96,41 @@
      |        Status Value n           |
      +---------------------------------+
   */
- 
- typedef enum {
-     PROTOCOL_PACKET_STATUS_TYPE_WIFI = 0x00, // 0x00 State of Wi-Fi connection, 0x01 = connected, 0x00 = disconnected
-     PROTOCOL_PACKET_STATUS_TYPE_GDB_CLIENT,      // 0x01 GDB client connected, 0x01 = connected, 0x00 = disconnected
-     PROTOCOL_PACKET_STATUS_TYPE_UART_CLIENT,    // 0x02 UART client connected, 0x01 = connected, 0x00 = disconnected
-     PROTOCOL_PACKET_STATUS_TYPE_SWO_CLIENT,     // 0x03 SWO client connected, 0x01 = connected, 0x00 = disconnected 
-     PROTOCOL_PACKET_STATUS_TYPE_NETWORK_CLIENT, // 0x04 Network data structure describes the current network
- } protocol_packet_status_type_e;
- 
- #define M2M_MAX_SSID_LEN 33 // Maximum length of the SSID string
- 
- /**
+
+typedef enum {
+	PROTOCOL_PACKET_STATUS_TYPE_WIFI = 0x00,    // 0x00 State of Wi-Fi connection, 0x01 = connected, 0x00 = disconnected
+	PROTOCOL_PACKET_STATUS_TYPE_GDB_CLIENT,     // 0x01 GDB client connected, 0x01 = connected, 0x00 = disconnected
+	PROTOCOL_PACKET_STATUS_TYPE_UART_CLIENT,    // 0x02 UART client connected, 0x01 = connected, 0x00 = disconnected
+	PROTOCOL_PACKET_STATUS_TYPE_SWO_CLIENT,     // 0x03 SWO client connected, 0x01 = connected, 0x00 = disconnected
+	PROTOCOL_PACKET_STATUS_TYPE_NETWORK_CLIENT, // 0x04 Network data structure describes the current network
+} protocol_packet_status_type_e;
+
+#define M2M_MAX_SSID_LEN 33 // Maximum length of the SSID string
+
+/**
   * @brief Structure used to hold network connection data
   * 
   */
- typedef struct {
-    protocol_packet_status_type_e type; // Type of status packet
-     char network_ssid[M2M_MAX_SSID_LEN]; // AP connection SSID name.  Only valid in
-                                    //  station mode.  Will be NULL in SoftAP
-                                    //  mode or P2P mode.
-     uint8_t ip_address[4];          // connection IP address
-     uint8_t mac_address[6];      // MAC address of the peer Wi-Fi station
-     int8_t rssi;  
- } network_connection_info_s;
- 
- typedef struct {
-    protocol_packet_status_type_e type; // Type of status packet
-     uint8_t status; // 0x00 = disconnected, 0x01 = connected
- } protocol_packet_status_s;
- 
- size_t package_data(uint8_t * buffer, size_t data_length, protocol_packet_type_e data_type, size_t buffer_size);
- void protocol_split(uint8_t *message, size_t *packet_size, protocol_packet_type_e *packet_type, uint8_t **data) ;
- 
- #ifdef __cplusplus
- }
- #endif
- 
- #endif // PROTOCOL_H
+typedef struct {
+	protocol_packet_status_type_e type;  // Type of status packet
+	char network_ssid[M2M_MAX_SSID_LEN]; // AP connection SSID name.  Only valid in
+										 //  station mode.  Will be NULL in SoftAP
+										 //  mode or P2P mode.
+	uint8_t ip_address[4];               // connection IP address
+	uint8_t mac_address[6];              // MAC address of the peer Wi-Fi station
+	int8_t rssi;
+} network_connection_info_s;
+
+typedef struct {
+	protocol_packet_status_type_e type; // Type of status packet
+	uint8_t status;                     // 0x00 = disconnected, 0x01 = connected
+} protocol_packet_status_s;
+
+size_t package_data(uint8_t *buffer, size_t data_length, protocol_packet_type_e data_type);
+size_t protocol_split(uint8_t *message, size_t *packet_size, protocol_packet_type_e *packet_type, uint8_t **data);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // PROTOCOL_H
